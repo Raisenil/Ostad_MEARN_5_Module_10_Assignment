12aa9872c479370abc0d59fec460982dc9589056
MD. Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2024-02-06 22:24:49 +0600

    Typo fixed

--- Assignment/index.js
+++ Assignment/index.js
@@ -5,31 +5,31 @@ var multer = require('multer')
 http.createServer(function(req,res){
     if (req.url=="/") {
         fs.readFile('home.html',function(error,data) {
-            res.writeHead(200,{'content-Type':'text/html'})
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         });
     }else if(req.url=="/about"){
         fs.readFile('about.html',function(error,data) {
-            res.writeHead(200,{'content-Type':'text/html'})
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         });
     }else if(req.url=="/contact"){
         fs.readFile('contact.html',function(error,data) {
-            res.writeHead(200,{'content-Type':'text/html'})
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         });
     }else if(req.url=="/file-write"){
         fs.writeFile('demo.txt','hello world',function(error){
             if(error){
-                res.writeHead(404,{'content-Type':'text/html'})
+                res.writeHead(404,{'Content-Type':'text/html'})
                 res.write("File Write Fail")
                 res.end()
             }else{
                 fs.readFile('file-write.html',function(error,data) {
-                    res.writeHead(200,{'content-Type':'text/html'})
+                    res.writeHead(200,{'Content-Type':'text/html'})
                     res.write(data)
                     res.end()
                 });
@@ -37,7 +37,7 @@ http.createServer(function(req,res){
         })
     }else if(req.url == "/file-upload"){
         fs.readFile('upload.html',function(error,data) {
-            res.writeHead(200,{'content-Type':'text/html'})
+            res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end()
         });
@@ -64,14 +64,14 @@ http.createServer(function(req,res){
                 fs.readFile('upload.html',function(error,data) {
                     let htmlContent = data.toString();
                     htmlContent = htmlContent.replace('d-none', '');
-                    res.writeHead(200,{'content-Type':'text/html'})
+                    res.writeHead(200,{'Content-Type':'text/html'})
                     res.write(htmlContent)
                     res.end()
                 });
             }
         });
     }else{
-        res.writeHead(404,{'content-Type':'text/html'})
+        res.writeHead(404,{'Content-Type':'text/html'})
         res.write(`<h1>Page Not Found</h1>`)
         res.end()
     }
